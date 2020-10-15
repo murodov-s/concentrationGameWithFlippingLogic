fd2cb778660f0bb6586acae409c0ab07bf9251f6
Safarlibek Murodov <murodov_s@auca.kg>
2020-10-16 02:25:34 +0530

    Created GUI and the flipping logic for Concentration Game. The way of solving it a bit straightforward, will work on it.

--- ConcentrationGameWithFlippingCardsLogic/ViewController.swift
+++ ConcentrationGameWithFlippingCardsLogic/ViewController.swift
@@ -14,6 +14,157 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func handleCardButtonTap1(_ sender: UIButton) {
+        if sender.currentTitle?.count == 0 {
+                    sender.setTitle("🛵", for: .normal)
+                    sender.backgroundColor = .white
+                } else {
+                    sender.setTitle("", for: .normal)
+                    sender.backgroundColor = #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1)
+                }
+    }
+    @IBAction func handleCardButtonTap2(_ sender: UIButton) {
+        if sender.currentTitle?.count == 0 {
+                    sender.setTitle("🍔", for: .normal)
+                    sender.backgroundColor = .white
+                } else {
+                    sender.setTitle("", for: .normal)
+                    sender.backgroundColor = #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1)
+                }
+    }
+    
+    @IBAction func handleCardButtonTap3(_ sender: UIButton) {
+        if sender.currentTitle?.count == 0 {
+                    sender.setTitle("🍑", for: .normal)
+                    sender.backgroundColor = .white
+                } else {
+                    sender.setTitle("", for: .normal)
+                    sender.backgroundColor = #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1)
+                }
+        
+    }
+    
+    @IBAction func handleCardButtonTap4(_ sender: UIButton) {
+        if sender.currentTitle?.count == 0 {
+                    sender.setTitle("🍆", for: .normal)
+                    sender.backgroundColor = .white
+                } else {
+                    sender.setTitle("", for: .normal)
+                    sender.backgroundColor = #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1)
+                }
+    }
+    @IBAction func handleCardButtonTap5(_ sender: UIButton) {
+        if sender.currentTitle?.count == 0 {
+                    sender.setTitle("🍄", for: .normal)
+                    sender.backgroundColor = .white
+                } else {
+                    sender.setTitle("", for: .normal)
+                    sender.backgroundColor = #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1)
+                }
+    }
+    
+    @IBAction func handleCardButtonTap6(_ sender: UIButton) {
+        if sender.currentTitle?.count == 0 {
+                    sender.setTitle("⚽️", for: .normal)
+                    sender.backgroundColor = .white
+                } else {
+                    sender.setTitle("", for: .normal)
+                    sender.backgroundColor = #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1)
+                }
+    }
+    @IBAction func handleCardButtonTap7(_ sender: UIButton) {
+        if sender.currentTitle?.count == 0 {
+                    sender.setTitle("🧄", for: .normal)
+                    sender.backgroundColor = .white
+                } else {
+                    sender.setTitle("", for: .normal)
+                    sender.backgroundColor = #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1)
+                }
+    }
+    @IBAction func handleCardButtonTap8(_ sender: UIButton) {
+        if sender.currentTitle?.count == 0 {
+                    sender.setTitle("🌎", for: .normal)
+                    sender.backgroundColor = .white
+                } else {
+                    sender.setTitle("", for: .normal)
+                    sender.backgroundColor = #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1)
+                }
+        
+    }
+    @IBAction func handleCardButtonTap9(_ sender: UIButton) {
+        if sender.currentTitle?.count == 0 {
+                    sender.setTitle("🎾", for: .normal)
+                    sender.backgroundColor = .white
+                } else {
+                    sender.setTitle("", for: .normal)
+                    sender.backgroundColor = #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1)
+                }
+        
+    }
+    @IBAction func handleCardButtonTap10(_ sender: UIButton) {
+        if sender.currentTitle?.count == 0 {
+                    sender.setTitle("🎁", for: .normal)
+                    sender.backgroundColor = .white
+                } else {
+                    sender.setTitle("", for: .normal)
+                    sender.backgroundColor = #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1)
+                }
+    }
+    @IBAction func handleCardButtonTap11(_ sender: UIButton) {
+        if sender.currentTitle?.count == 0 {
+                    sender.setTitle("🦁", for: .normal)
+                    sender.backgroundColor = .white
+                } else {
+                    sender.setTitle("", for: .normal)
+                    sender.backgroundColor = #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1)
+                }
+    }
+    @IBAction func handleCardButtonTap12(_ sender: UIButton) {
+        if sender.currentTitle?.count == 0 {
+                    sender.setTitle("🎩", for: .normal)
+                    sender.backgroundColor = .white
+                } else {
+                    sender.setTitle("", for: .normal)
+                    sender.backgroundColor = #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1)
+                }
+        
+    }
+    @IBAction func handleCardButtonTap13(_ sender: UIButton) {
+        if sender.currentTitle?.count == 0 {
+                    sender.setTitle("🍎", for: .normal)
+                    sender.backgroundColor = .white
+                } else {
+                    sender.setTitle("", for: .normal)
+                    sender.backgroundColor = #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1)
+                }
+    }
+    @IBAction func handleCardButtonTap14(_ sender: UIButton) {
+        if sender.currentTitle?.count == 0 {
+                    sender.setTitle("🎲", for: .normal)
+                    sender.backgroundColor = .white
+                } else {
+                    sender.setTitle("", for: .normal)
+                    sender.backgroundColor = #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1)
+                }
+    }
+    
+    @IBAction func handleCardButtonTap15(_ sender: UIButton) {
+        if sender.currentTitle?.count == 0 {
+                    sender.setTitle("🍅", for: .normal)
+                    sender.backgroundColor = .white
+                } else {
+                    sender.setTitle("", for: .normal)
+                    sender.backgroundColor = #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1)
+                }
+    }
+    @IBAction func handleCardButtonTap16(_ sender: UIButton) {
+        if sender.currentTitle?.count == 0 {
+                    sender.setTitle("🍿", for: .normal)
+                    sender.backgroundColor = .white
+                } else {
+                    sender.setTitle("", for: .normal)
+                    sender.backgroundColor = #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1)
+                }
+    }
 }
 
